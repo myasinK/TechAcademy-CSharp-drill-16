593dad64176ca260af5d4f4e5883fd91f0ed7b66
myasinK <yasinul@gmail.com>
2018-08-15 21:00:55 -0700

    Finish fifth drill on classes

---
+++ MoreOnClasses/CreateClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreOnClasses
+{
+    public static class CreateClass
+    {
+        public static void Func(decimal x)
+        {
+            Console.WriteLine( Decimal.ToInt64( x/2 ) );
+        }
+        static int Func(int x, int y)
+        {
+            return x + y;
+        } 
+    }
+    public class CreateClass2
+    {
+        public void Func(decimal x)
+        {
+            Console.WriteLine(Decimal.ToInt64(x / 2));
+        }
+        public int Func(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ MoreOnClasses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreOnClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateClass2 createClass2 = new CreateClass2();
+            Console.WriteLine("Enter a number");
+            decimal num = Convert.ToDecimal( Console.ReadLine() );
+            int newNum = (int)num;
+            createClass2.Func( newNum, newNum + 1 );
+
+            CreateClass.Func(num);
+            
+        }
+    }
+}
